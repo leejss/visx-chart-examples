968c66d5da6c8932b497dff86738f26a1490ab98
leejss <leejss@neowiz.com>
2024-12-12 15:49:51 +0900

    add line chart

--- README.md
+++ README.md
@@ -3,3 +3,38 @@
 ## Animating path
 
 - 컴포넌트가 마운트 될때 Path가 그려지는 애니메이션을 구현해보자.
+
+## Drawing path
+
+### LinePath
+
+```tsx
+
+<LinePath data={data} x={(d) => xScale(getDate(d)) ?? 0} y={(d) => yScale(getValue(d)) ?? 0} />
+
+```
+
+### line function
+
+```tsx
+
+const createLinePath = (data: DataPoint[]) => {
+  return line<DataPoint>()
+    .x((d) => xScale(getDate(d)) ?? 0)
+    .y((d) => yScale(getValue(d)) ?? 0)
+    .curve(curveMonotoneX);
+};
+
+// Data를 받아서 라인 패스를 생성하는 함수
+<g>
+  <path d={createLinePath(data)} stroke="#4f46e5" strokeWidth={2} />
+</g>
+```
+
+## Interactivity
+
+### Tooltip
+
+- 포인터의 x, y 좌표를 얻는다.
+- 그리고 포인터 위치에 대응하는 데이터를 얻는다.
+- rect 요소를 통해서 hover area를 설정한다.
--- src/examples/LineChartExample2.tsx
+++ src/examples/LineChartExample2.tsx
@@ -2,9 +2,17 @@ import { Group } from "@visx/group";
 import { LinePath } from "@visx/shape";
 import { DataPoint, getExampleData } from "../lib/data";
 import { scaleLinear } from "@visx/scale";
-import { extent } from "d3-array";
+import { bisector, extent } from "d3-array";
 import { AxisBottom, AxisLeft } from "@visx/axis";
 import { ParentSize } from "@visx/responsive";
+import { TooltipWithBounds, useTooltip, defaultStyles } from "@visx/tooltip";
+import { useCallback } from "react";
+import { localPoint } from "@visx/event";
+import { GlyphCircle } from "@visx/glyph";
+
+// TODO: add tooltip
+// TODO: Understanding how showing a tooltip works
+// TODO: add point
 
 const getDate = (d: DataPoint) => d.date;
 const getValue = (d: DataPoint) => d.value;
@@ -14,12 +22,20 @@ type LineChartProps = {
   height: number;
 };
 
+const bisectDate = bisector<DataPoint, Date>(getDate).left;
+const margin = { top: 20, right: 20, bottom: 50, left: 50 };
+const { basicData } = getExampleData();
 function LineChart({ width, height }: LineChartProps) {
-  const { basicData } = getExampleData();
-  const margin = { top: 20, right: 20, bottom: 50, left: 50 };
+  const {
+    tooltipData,
+    tooltipLeft = 0,
+    tooltipTop = 0,
+    showTooltip,
+    hideTooltip,
+  } = useTooltip<DataPoint>();
 
-  const innerWidth = width - margin.left - margin.right;
-  const innerHeight = height - margin.top - margin.bottom;
+  const innerWidth = Math.max(0, width - margin.left - margin.right);
+  const innerHeight = Math.max(0, height - margin.top - margin.bottom);
 
   // Scale creation - transforms data values to pixel values
   const xScale = scaleLinear({
@@ -33,6 +49,37 @@ function LineChart({ width, height }: LineChartProps) {
     domain: extent(basicData, getValue) as [number, number],
     nice: true,
   });
+
+  const handleTooltip = useCallback(
+    (event: React.MouseEvent | React.TouchEvent) => {
+      // Get coordinates from the event
+      const { x } = localPoint(event) || { x: 0, y: 0 };
+      // Get the data from the x coordinate
+      const date = xScale.invert(x);
+      // Get the index of the data from the x0
+      const index = bisectDate(basicData, new Date(date));
+      // Get the data from the index
+
+      const d0 = basicData[index - 1];
+      const d1 = basicData[index];
+      let d = d0;
+
+      if (d1 && getDate(d1)) {
+        d =
+          date.valueOf() - getDate(d0).valueOf() >
+          getDate(d1).valueOf() - date.valueOf()
+            ? d1
+            : d0;
+      }
+
+      showTooltip({
+        tooltipData: d,
+        tooltipLeft: xScale(getDate(d)) + margin.left,
+        tooltipTop: yScale(getValue(d)) + margin.top,
+      });
+    },
+    [xScale, yScale, showTooltip],
+  );
 
   return (
     <svg width={width} height={height}>
@@ -56,6 +103,52 @@ function LineChart({ width, height }: LineChartProps) {
           label="Date"
           labelOffset={40}
         />
+
+        {tooltipData ? (
+          <TooltipWithBounds
+            key={Math.random()}
+            top={tooltipTop}
+            left={tooltipLeft}
+            style={{
+              ...defaultStyles,
+              // backgroundColor: "black",
+              // border: "1px solid #ccc",
+              // padding: "10px",
+              // color: "#fff",
+            }}
+          >
+            <p>{`Value: ${getValue(tooltipData).toFixed(2)}`}</p>
+            <p>{`Date: ${getDate(tooltipData)}`}</p>
+          </TooltipWithBounds>
+        ) : null}
+
+        {tooltipData && (
+          <g>
+            <GlyphCircle
+              left={xScale(getDate(tooltipData))}
+              top={yScale(getValue(tooltipData))}
+              size={110}
+              fill={"blue"}
+              stroke={"white"}
+              strokeWidth={2}
+            />
+          </g>
+        )}
+
+        {/* Hover Event area */}
+        <rect
+          x={0}
+          y={0}
+          width={innerWidth}
+          fill="transparent"
+          height={innerHeight}
+          onMouseMove={handleTooltip}
+          onTouchMove={handleTooltip}
+          onMouseLeave={() => {
+            hideTooltip();
+          }}
+          onTouchEnd={hideTooltip}
+        />
       </Group>
     </svg>
   );
@@ -64,7 +157,7 @@ function LineChart({ width, height }: LineChartProps) {
 // Wrapper component that makes the chart responsive
 export default function LineChartExample2() {
   return (
-    <div style={{ width: "100%", height: "500px" }}>
+    <div className="relative" style={{ width: "100%", height: "500px" }}>
       <ParentSize>
         {({ width, height }) => <LineChart width={width} height={height} />}
       </ParentSize>
